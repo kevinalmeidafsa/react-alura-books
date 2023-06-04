fa006ddf5282bce52f35132c8ef517cf7d35aa45
Kevin Almeida <kevin.almeidafsa@gmail.com>
2023-06-04 12:20:59 -0300

    Resultado da pesquisa :tada:

---
+++ src/components/Search-input/dataSearch.js
@@ -0,0 +1,59 @@
+import book1 from "../../imagens/Book1.jpg";
+import book2 from "../../imagens/Book2.jpg";
+import book3 from "../../imagens/Book3.jpg";
+import book4 from "../../imagens/Book4.jpg";
+import book5 from "../../imagens/Book5.jpg";
+import book6 from "../../imagens/Book6.jpg";
+import book7 from "../../imagens/Book7.jpg";
+import book8 from "../../imagens/Book8.jpg";
+import book9 from "../../imagens/Book9.jpg";
+
+const books = [
+  {
+    name: "The Curses",
+    id: 1,
+    src: {book1},
+  },
+  {
+    name: "Psychopath Test",
+    id: 2,
+    src: {book2},
+  },
+  {
+    name: "London Lies Beneath",
+    id: 3,
+    src: {book3},
+  },
+  {
+    name: "The Wicker Deep",
+    id: 4,
+    src: {book4},
+  },
+  {
+    name: "Classic Ghost Stories",
+    id: 5,
+    src: {book5},
+  },
+  {
+    name: "The Graces",
+    id: 6,
+    src: {book6},
+  },
+  {
+    name: "Eletric Unicorn",
+    id: 7,
+    src: {book7},
+  },
+  {
+    name: "Harry Potter and the Philosophers Stone",
+    id: 8,
+    src: { book8 },
+  },
+  {
+    name: "Harry Potter and the Chamber of Secrets",
+    id: 9,
+    src: { book9 },
+  },
+];
+
+export default books;
--- src/components/Search-input/index.js
+++ src/components/Search-input/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Input from "../Input";
 import { useState } from "react";
+import books from "./dataSearch";
 
 const SearchContainer = styled.section`
   text-align: center;
@@ -14,17 +15,27 @@ const TextSizeMedium = styled.p`
 `;
 
 function SearchInput() {
-  const [typedText, settypedText] = useState("");
-
+  const [searchedBooks, setsearchedBooks] = useState([]);
   return (
     <SearchContainer>
       <h2>Já sabe por onde começar?</h2>
       <TextSizeMedium>Encontre seu livro em nossa loja.</TextSizeMedium>
       <Input
         placeholder="Escreva o nome do livro"
-        onBlur={userSearch => settypedText(userSearch.target.value)}
+        onBlur={(userSearch) => {
+          const typedText = userSearch.target.value;
+          const searchResult = books.filter((book) =>
+            book.name.includes(typedText)
+          );
+          setsearchedBooks(searchResult);
+        }}
       />
-      <p>{typedText}</p>
+      {searchedBooks.map((books) => (
+        <div>
+          <p>{books.name}</p>
+          <img src={books.src} alt={books.name} />
+        </div>
+      ))}
     </SearchContainer>
   );
 }
